9d472ed631310cad447f2e7557c8859f61e758fb
itsKat <77287432+itsKatVR@users.noreply.github.com>
2024-05-14 01:45:13 -0400

    Add files via upload

---
+++ AssetRelocationPlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class AssetRelocationPlayerCollision : UdonSharpBehaviour
+{
+    public GameObject[] objects; // Array of Objects
+    public GameObject[] placements; // Array of Locations
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        // Assuming Order of Objects & Locations match.
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].transform.position = placements[i].transform.position;
+            objects[i].transform.rotation = placements[i].transform.rotation;
+        }
+    }
+}
